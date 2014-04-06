9efa2761514b8211ac094da4b20131a1007c2228
Etienne Rossignon <etienne.rossignon@gadz.org>
2014-04-06 10:09:49 +0200

    add subscription and monitored item example

--- simple_client.js
+++ simple_client.js
@@ -44,13 +44,78 @@ async.series([
     },
     // step 4 : read a variable
     function(callback) {
-        the_session.read("ns=2;s=Furnace_1.Temperature", function(err,dataValues,diagnostics) {
+        the_session.readVariableValue("ns=2;s=Furnace_1.Temperature", function(err,dataValues,diagnostics) {
             if (!err) {
                 console.log(" temperature = " , dataValues[0].value.value);
             }
             callback(err);
         })
     },
+
+    // step 5: install a subscription and monitored item
+    //
+    // -----------------------------------------
+    // create subscription
+    function(callback) {
+
+        the_subscription=new opcua.ClientSubscription(the_session,{
+            requestedPublishingInterval: 1000,
+            requestedLifetimeCount: 10,
+            requestedMaxKeepAliveCount: 2,
+            maxNotificationsPerPublish: 10,
+            publishingEnabled: true,
+            priority: 10
+        });
+        the_subscription.on("started",function(){
+            console.log("subscription started for 2 seconds - subscriptionId=",the_subscription.subscriptionId);
+        }).on("keepalive",function(){
+            console.log("keepalive");
+        }).on("terminated",function(){
+            callback();
+        });
+        setTimeout(function(){
+            the_subscription.terminate();
+        },10000);
+
+
+        // install monitored item
+        //
+        var monitoredItem  = the_subscription.monitor({
+            nodeId: opcua.resolveNodeId("ns=2;s=Furnace_1.Temperature"),
+            attributeId: 13
+          //, dataEncoding: { namespaceIndex: 0, name:null }
+        },
+        { 
+            samplingInterval: 100,
+            discardOldest: true,
+            queueSize: 10 
+        });
+        console.log("-------------------------------------");
+
+        // subscription.on("item_added",function(monitoredItem){
+        //xx monitoredItem.on("initialized",function(){ });
+        //xx monitoredItem.on("terminated",function(value){ });
+        
+
+        monitoredItem.on("changed",function(value){
+           console.log(" New Value = ",value.toString());
+        });
+
+    },
+
+    // ------------------------------------------------
+    // closing session
+    //
+    function(callback) {
+        console.log(" closing session");
+        the_session.close(function(err){
+
+            console.log(" session closed");
+            callback();
+        });
+    },
+
+
 ],
     function(err) {
         if (err) {
